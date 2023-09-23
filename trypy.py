c972677431f51c9577db752e9a3cc450f316721e
Midou <midou.ik.1999@gmail.com>
2023-09-23 16:44:46 +0200

    last commit for news

--- trypy.py
+++ trypy.py
@@ -1,3 +1,4 @@
+from newsapi import NewsApiClient  
 import numpy as np
 import pandas as pd 
 from sklearn.pipeline import Pipeline
@@ -5,6 +6,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score 
+import pickle 
+from datetime import datetime
+
+newsapi=NewsApiClient("")  
+
+
+
 
 
 with open('modelSVC.pkl', 'rb') as file:
@@ -25,7 +33,6 @@ def encode(senti):
     
 def news201(title , source=None): 
   today = datetime.today()
-  two_weeks_ago = today - timedelta(days=7) 
   data=newsapi.get_everything (q=title , sources=source , language='en', 
             sort_by='relevancy', to=today, page_size=100) 
   artic=data['articles']   
